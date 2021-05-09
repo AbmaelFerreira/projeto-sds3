dd73668e125b634617f5770abda79bddb43e1a40
AwaitSistemas <abmael.ninha@gmail.com>
2021-05-09 07:30:28 -0400

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleServices.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsvendas.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleServices {
+
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		
+		Page<Sale> result = saleRepository.findAll(pageable);
+		
+		//return result.stream().map(x -> new SaleDTO(x)).collect(Collectors.toList()); para o java 8 não ha necessidade
+		return result.map(x -> new SaleDTO(x));
+		
+	}
+	
+	
+}
