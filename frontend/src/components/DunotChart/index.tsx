64ace0181903c40c0fa558d521821d28167625aa
AwaitSistemas <abmael.ninha@gmail.com>
2021-05-09 22:06:30 -0400

    DonutChart integration

--- frontend/src/components/DunotChart/index.tsx
+++ frontend/src/components/DunotChart/index.tsx
@@ -1,32 +1,42 @@
 import axios from 'axios';
 import { BASE_URL } from 'components/NavBar/utils/request';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 
 type ChartData = {
     labels: string[];
     series: number[];
-} 
+}
 
 const DunotChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+
+            });
+
+    }, []);
+
     //Forma errada
-    let chartData: ChartData =  {labels:[], series: []};
+    // let chartData: ChartData =  {labels:[], series: []};
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then((response) => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
 
-            chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData);
-        })
 
     // const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
     //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     // }
-    
+
     const options = {
         legend: {
             show: true
@@ -35,7 +45,7 @@ const DunotChart = () => {
 
     return (
         <Chart
-            options ={{...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
