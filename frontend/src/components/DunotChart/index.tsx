f3d53c6bf861d634b7b419e86e8fd4bde7e81282
AwaitSistemas <abmael.ninha@gmail.com>
2021-05-09 21:50:50 -0400

    First request

--- frontend/src/components/DunotChart/index.tsx
+++ frontend/src/components/DunotChart/index.tsx
@@ -1,10 +1,31 @@
+import axios from 'axios';
+import { BASE_URL } from 'components/NavBar/utils/request';
 import Chart from 'react-apexcharts'
-const DunotChart = () => {
+import { SaleSum } from 'types/sale';
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+type ChartData = {
+    labels: string[];
+    series: number[];
+} 
+
+const DunotChart = () => {
+    //Forma errada
+    let chartData: ChartData =  {labels:[], series: []};
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then((response) => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = {labels: myLabels, series: mySeries};
+            console.log(chartData);
+        })
+
+    // const mockData = {
+    //     series: [477138, 499928, 444867, 220426, 473088],
+    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    // }
     
     const options = {
         legend: {
@@ -14,8 +35,8 @@ const DunotChart = () => {
 
     return (
         <Chart
-            options ={{...options, labels: mockData.labels}}
-            series={mockData.series}
+            options ={{...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
 
